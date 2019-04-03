16b6436c7a5bcb2f992324f66e328e7de6417ba5
ryudung <rhkdgus221@naver.com>
2019-04-03 23:39:04 +0900

    데이터 추가

--- src/test/java/AssertionTest.java
+++ src/test/java/AssertionTest.java
@@ -1,10 +1,29 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
 public class AssertionTest {
+
+    private final TolkienCharacter frodo = new TolkienCharacter("Frodo", 33, Race.HOBBIT);
+    private final TolkienCharacter sam = new TolkienCharacter("Sam", 38, Race.HOBBIT);
+    private final TolkienCharacter legolas = new TolkienCharacter("Legolas", 1000, Race.ELF);
+    private final TolkienCharacter aragorn = new TolkienCharacter("Aragorn", 87, Race.MAN);
+    private final TolkienCharacter boromir = new TolkienCharacter("Boromir", 37, Race.MAN);
+    private final TolkienCharacter sauron = new TolkienCharacter("Sauron", 50000, Race.MAIA);
+
+    private List<TolkienCharacter> fellowshipOfTheRing;
+
+    @Before
+    public void before() {
+        fellowshipOfTheRing = Arrays.asList(frodo, sam, legolas, aragorn, boromir);
+    }
+
 
     @Test
     public void test1() {
@@ -39,11 +58,13 @@ public class AssertionTest {
     public void test5() {
 
         assertThatThrownBy(() -> {
-
+            throw new Exception("boom!");
         }).hasMessage("boom!");
 
 
-        Throwable thrown = catchThrowable();
+        Throwable thrown = catchThrowable(() -> {
+            throw new Exception("boom!");
+        });
 
         assertThat(thrown).hasMessageContaining("boom");
 
